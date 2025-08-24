0bcd423bbdb7a5c21d7d2b99a6c0fd8ffdf70ff0
eerytea <ryanpatrickmadigan@gmail.com>
2025-08-24 17:56:00 -0500

    Update __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,15 +1,6 @@
-from .app import App
-from .state_menu import MenuState
-from .state_team_select import TeamSelectState
-from .state_season_hub import SeasonHubState
-from .state_exhibition import ExhibitionState
-from .state_settings import SettingsState
-from .state_message import MessageState
 # ui/__init__.py
-from __future__ import annotations
 from importlib import import_module
 
-# Only list the symbols you actually want to expose
 __all__ = [
     "App",
     "MenuState",
