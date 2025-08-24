6e9461b4282b83064e4ff42cf3f1dea77f2b3343
eerytea <ryanpatrickmadigan@gmail.com>
2025-08-24 15:01:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+# main.py
 from ui.app import App
 from ui.state_menu import MenuState
 
 def main():
-    app = App()
-    app.push_state(MenuState())
+    app = App(width=1280, height=720, title="D20 Fight Club")
+    app.push_state(MenuState())  # <-- make sure we push the start screen
     app.run()
 
 if __name__ == "__main__":
